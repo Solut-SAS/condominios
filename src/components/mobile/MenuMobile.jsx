04e9565b421d4f53c93f24ef4fe0a16fc97c2a5a
Laura Vanessa Zuluaga <49324952+LauraVZuluaga@users.noreply.github.com>
2023-05-05 21:36:24 -0500

    First responsive menu structure

--- src/components/mobile/MenuMobile.jsx
+++ src/components/mobile/MenuMobile.jsx
@@ -1,22 +1,39 @@
-import { box } from "../../assets";
+import { logo } from "../../assets";
+import { HiOutlineX } from "react-icons/hi";
+import { menu } from "../../data/menu";
 
-const MenuMobile = () => {
+const MenuMobile = ({ changeMobileNav }) => {
   return (
-    <div className="fixed bottom-0 mt-8 bg-white flex flex-row justify-evenly w-full h-[9vh] border rounded-t-lg drop-shadow-md  z-50">
-      <div className="flex flex-col justify-center w-1/4">
-        <img src={box} width={"18%"} className="self-center text-center" />
-        <span className="text-center">Paquetes</span>
+    <div className="bg-white w-full h-full">
+      <div className="p-5 flex justify-between">
+        <a href="commerces" data-aos="fade-down" data-aos-delay="1000">
+          <img src={logo} alt="logo" className="w-16" />
+        </a>
+        <HiOutlineX
+          className="text-3xl text-secondary mr-2 mt-2"
+          onClick={() => changeMobileNav(false)}
+        />
       </div>
-
-      <div className="flex flex-col justify-center w-1/4">
-        <img src={box} width={"18%"} className="self-center text-center" />
-        <span className="text-center">Invitaciones</span>
-      </div>
-
-      <div className="flex flex-col justify-center w-1/4">
-        <img src={box} width={"18%"} className="self-center text-center" />
-        <span className="text-center">Comunicados</span>
-      </div>
+      <ul className="h-full flex flex-col justify-center items-center align-middle gap-y-8 -mt-20">
+        {menu.map((item, index) => {
+          const { path, name } = item;
+          return (
+            <li key={index} onClick={() => changeMobileNav(false)}>
+              <a
+                className="link text-secondary text-lg font-semibold"
+                href={path}
+              >
+                {name}
+              </a>
+            </li>
+          );
+        })}
+      </ul>
+      <ul className="h-full flex justify-between -mt-12 px-10 text-slate-400">
+        <li>Facebook</li>
+        <li>Instagram</li>
+        <li>LinkedIn</li>
+      </ul>
     </div>
   );
 };
--- src/layouts/AuthLayout.jsx
+++ src/layouts/AuthLayout.jsx
@@ -1,30 +1,48 @@
 import Footer from "./footer";
 import SideBar from "../components/sidebar/Sidebar";
 import MenuMobile from "../components/mobile/MenuMobile";
+import { HiMenuAlt4, HiOutlineX } from "react-icons/hi";
 import { logo } from "../assets";
-
-const LayoutWrapper = ({ children }) => (
-  <>
-    <div className="pl-0 pt-0 sm:pl-6 sm:pt-6">
-      {/* <div className="mb-0 hidden sm:mb-10 sm:block ml-8"> */}
-        <img src={logo} className={"w-1/12 ml-8"} />
-      {/* </div> */}
-      <div className={"flex flex-row"}>
-        <div className="hidden sm:block">
-          <SideBar />
-        </div>
-        <main className="w-full mb-10">{children}</main>
-      </div>
-    </div>
-    <div className="block md:hidden">
-      <MenuMobile />
-    </div>
-    {/* <Footer /> */}
-  </>
-);
+import { useState } from "react";
 
 const AuthLayout = ({ children }) => {
-  return <LayoutWrapper>{children}</LayoutWrapper>;
+  const [mobileNav, setMobileNav] = useState(false);
+
+  const changeMobileNav = (mobileNavStatus) => {
+    console.log(mobileNavStatus, "holi");
+    setMobileNav(mobileNavStatus);
+    console.log(mobileNav, "holi 2");
+  };
+
+  return (
+    <div>
+      <div className="pl-0 pt-0 sm:pl-6 sm:pt-6">
+        {/* <div className="mb-0 hidden sm:mb-10 sm:block ml-8"> */}
+        <img src={logo} className={"w-1/12 ml-8"} />
+        {/* </div> */}
+        <div className={"flex flex-row"}>
+          <div className="hidden sm:block">
+            <SideBar />
+          </div>
+          {/* <main className="w-full mb-10">{children}</main> */}
+        </div>
+      </div>
+      <button className="lg:hidden" onClick={() => setMobileNav(!mobileNav)}>
+        {mobileNav ? (
+          <HiOutlineX className="text-3xl text-secondary mr-2" />
+        ) : (
+          <HiMenuAlt4 className="text-3xl text-secondary mr-2" />
+        )}
+      </button>
+      <div
+        className={`${
+          mobileNav ? "left-0" : "-left-full"
+        } fixed top-0 bottom-0 w-[100vw] lg:hiddeb transition-all`}
+      >
+        <MenuMobile changeMobileNav={changeMobileNav} />
+      </div>
+    </div>
+  );
 };
 
 export default AuthLayout;
