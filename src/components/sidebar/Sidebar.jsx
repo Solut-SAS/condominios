1ff05a4d8c0e19bfe45e4bc94852e484dd0f8fe5
AndresMaya13 <andresmaya3113@gmail.com>
2023-11-26 09:30:44 -0500

    permisos en sidebar

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -2,10 +2,17 @@ import { Sidebar, Tooltip } from "flowbite-react";
 import { menu } from "../../data/menu";
 import { useNavigate } from "react-router-dom";
 
+import { useEffect, useState } from "react";
 import { premium } from "../../assets";
 
 function SidebarComponent() {
   let navigate = useNavigate();
+  const [user, setUser] = useState({});
+
+  useEffect(() => {
+    let user = JSON.parse(window.localStorage.getItem("user"));
+    setUser(user);
+  }, []);
 
   const handleLogout = () => {
     console.log("logout");
@@ -15,6 +22,17 @@ function SidebarComponent() {
     window.location.reload();
   };
 
+  const canAccess = (roles) => {
+    let user = JSON.parse(window.localStorage.getItem("user"));
+    if (user) {
+      let rol = user.rol;
+      if (roles.includes(rol)) {
+        return true;
+      }
+    }
+    return false;
+  };
+
   return (
     <Sidebar aria-label="Menu navigation condominios">
       <Sidebar.Items>
@@ -22,20 +40,21 @@ function SidebarComponent() {
           {menu.map((el) => (
             <Sidebar.Item
               key={el.name}
-              // href={el.path}
               onClick={() => {
-                el.status != 'inactive' && navigate(el.path);
+                el.status != "inactive" && navigate(el.path);
               }}
               className="text-lg font-medium tracking-tighter mb-4 hover:text-red-500 hover:cursor-pointer flex flex-row"
             >
-              <div className="flex flex-row">
-                <span className="mr-4">{el.name}</span>
-                {el.status == "inactive" && (
-                  <Tooltip content="Esta funcionalidad es premium, comunícate con tu asesor">
-                    <img src={premium} width={27}></img>
-                  </Tooltip>
-                )}
-              </div>
+              {canAccess(el.roles) && (
+                <div className="flex flex-row">
+                  <span className="mr-4">{el.name}</span>
+                  {el.status == "inactive" && (
+                    <Tooltip content="Esta funcionalidad es premium, comunícate con tu asesor">
+                      <img src={premium} width={27}></img>
+                    </Tooltip>
+                  )}
+                </div>
+              )}
             </Sidebar.Item>
           ))}
         </Sidebar.ItemGroup>
